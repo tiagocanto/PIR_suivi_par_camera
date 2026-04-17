13fc7d79d440590ef266a556f5e37023d9cb51db
Gabriela <gabriela.donzelli@ufu.br>
2026-04-17 12:04:47 +0200

    Refactor save_frames function to include start index
    Updated the frame saving logic to include a start index and modified the naming convention for saved frames. Adjusted the ratio of frames to be saved and added a list of videos for processing.

--- codes/TakeFrames.py
+++ codes/TakeFrames.py
@@ -1,33 +1,50 @@
-######
-# a 18secs video had ~1100 frames
-
 import cv2
 import os
 
-##defines the amount of frames by video
-ratio = 24
+# defines the amount of frames skipped between saves
+ratio = 110
 
-def save_frames(video_path, folder_dest, ratio):
+def save_frames(video_path, folder_dest, ratio, start_index=226):
+
     cap = cv2.VideoCapture(video_path)
+
     if not cap.isOpened():
-        print(f"Error : it was not possible open the video:\n{video_path}")
-        return
+        print(f"Error: it was not possible to open the video:\n{video_path}")
+        return start_index
+
     cont = 0
     saved = 0
+    current_index = start_index
+
     while True:
         ret, frame = cap.read()
         if not ret:
             break
+
         if cont % ratio == 0:
-            var_newName = os.path.join(folder_dest, f"img_{(cont//ratio)}.png")
+            var_newName = os.path.join(folder_dest, f"img_{current_index}.png")
             cv2.imwrite(var_newName, frame)
             saved += 1
+            current_index += 1
+
         cont += 1
+
     cap.release()
-    print(f"Done: {saved} frames saved on '{folder_dest}'.")
+    print(f"Done: {saved} frames saved from '{video_path}' into '{folder_dest}'.")
+    return current_index
 
-# EXEMPLO DE USO
-video = r"PUT_THE_VIDEO_PATH"
-folder = r"PUT_THE_FOLDER_PATH"
 
-save_frames(video, folder, ratio)
+# list with all videos to be framed
+videos = [
+    rf"C:MYPATH\V{i}.mp4"
+    for i in range(1, 12) ## from portable 2
+]
+
+# destination folder
+folder = r"C:MYPATH"
+
+# to name the images
+index_start = 672
+
+for video in videos:
+    index_start = save_frames(video, folder, ratio, start_index=index_start)
